c365dc09c7c161ab7905ff9c94870b4eec81da4f
Jacoblynch99 <jacoblynch99@gmail.com>
2020-09-20 15:38:35 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const _ = require("underscore")
 
-const numArr = [25, 693, 89, 7, 1.05, 15608, 50, 63]
+const numArr = [25, 693, 89, 7, 1]
+
+const returnNum = (num) => {
+  return num
+}
+
+const maxNum = _.max(numArr, returnNum) // returns the highest value number in a list. Does not change the original. dat
+
+const minNum = _.min(numArr, returnNum) // returns the lowest value number in a list. Does not change the original.
+
+console.log(`Underscore Data:`, maxNum)
+console.log(`Original Data:`, numArr)
+console.log(
+  `*****************************************************************************************`
+)
+console.log(`Underscore Data:`, minNum)
+console.log(`Original Data:`, numArr)
